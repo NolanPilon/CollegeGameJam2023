cd1275e8c947116869bb1d8fd4bb0de8945cc7b3
Nolan <npilon2003@gmail.com>
2023-10-23 17:53:55 -0400

    Added State Machine and Shrink enemy Function

--- Assets/Scripts/EnemyScripts/ConsumableWander.cs
+++ Assets/Scripts/EnemyScripts/ConsumableWander.cs
@@ -5,27 +5,50 @@ using UnityEngine;
 
 public class ConsumableWander : MonoBehaviour
 {
-    [SerializeField] private float wanderSpeed = 1.0f;
-
-    [SerializeField] private Vector2 target = new Vector2(0, 0);
-   
-    void Start()
+    enum AI_BEHAVIOUR 
     {
-        PickDirection();
+        WANDER = 0,
+        CHASE,
+
+        BOTTOM_TEXT
+    }
+
+    [SerializeField] private float wanderSpeed = 1.0f;
+    [SerializeField] private Vector2 target = new Vector2(0, 0);
+
+    [SerializeField] private int state = (int)AI_BEHAVIOUR.WANDER;
+
+    private GameObject playerRef = null;
+
+    private float distanceToPlayer = 1.0f;
+    private float fleeDistance = 5.0f;
+
+    void Start()
+    { 
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        PickRandomDirection();
     }
 
     private void FixedUpdate()
     {
-        if (transform.localPosition.x >= target.x && transform.localPosition.y <= target.y || 
-            transform.localPosition.x <= target.x && transform.localPosition.y >= target.y)
+        if (state == (int)AI_BEHAVIOUR.WANDER) 
         {
-            PickDirection();
+            if (transform.localPosition.x >= target.x && transform.localPosition.y <= target.y ||
+                transform.localPosition.x <= target.x && transform.localPosition.y >= target.y)
+            {
+                PickRandomDirection();
+            }
+
+            transform.localPosition = Vector2.MoveTowards(transform.localPosition, target, wanderSpeed * Time.deltaTime);
         }
-        
-        transform.localPosition = Vector2.MoveTowards(transform.localPosition, target, wanderSpeed * Time.deltaTime);
+
+        if (state == (int)AI_BEHAVIOUR.CHASE) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerRef.transform.position, wanderSpeed * Time.deltaTime);
+        }
     }
 
-    void PickDirection() 
+    void PickRandomDirection() 
     {
         target = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
     }

6ec70b26af4bd954380a1194156d51c629dffd3c
Nolan Pilon [Student] <nolan.pilon@Student.SL.On.Ca>
2023-10-23 11:10:51 -0400

    Added wander ai and temp camera follow

---
+++ Assets/Scripts/ConsumeEvent.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ConsumeEvent : MonoBehaviour
+{
+    private Transform playerTransform = null;
+
+
+    UnityEvent m_EatObject;
+    void Start()
+    {
+        if (m_EatObject == null) 
+        {
+            m_EatObject = new UnityEvent();
+        }
+
+        m_EatObject.AddListener(ConsumeObject);
+
+
+        playerTransform = GetComponent<Transform>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        m_EatObject.Invoke();
+        Destroy(collision.gameObject);
+    }
+
+    //Pass through object info later
+    void ConsumeObject() 
+    {
+        playerTransform.localScale += new Vector3(0.5f, 0.5f, 0);
+        Debug.Log("YUUUUUM!!!");
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/ConsumableWander.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ConsumableWander : MonoBehaviour
+{
+    [SerializeField] private float wanderDirectionTime = 1.0f;
+    [SerializeField] private float wanderSpeed = 1.0f;
+
+    [SerializeField] private Vector2 target = new Vector2(0, 0);
+   
+
+
+    void Start()
+    {
+        PickDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.localPosition.x >= target.x && transform.localPosition.y <= target.y)
+        {
+            PickDirection();
+        }
+        
+        transform.localPosition = Vector2.MoveTowards(transform.localPosition, target, wanderSpeed * Time.deltaTime);
+    }
+
+    void PickDirection() 
+    {
+        target = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
+    }
+}

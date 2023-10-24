f6806b34bffb579dea132d672c85326a00231b02
Nolan <npilon2003@gmail.com>
2023-10-23 21:29:42 -0400

    Fixed zoom out bug

--- Assets/Scripts/PlayerScripts/PlayerLevelUp.cs
+++ Assets/Scripts/PlayerScripts/PlayerLevelUp.cs
@@ -21,23 +21,30 @@ public class PlayerLevelUp : MonoBehaviour
     {
         if (SizeManager.Instance.isFull())
         {
-          allowedToGrow = true;
-          playerStartingSize = playerTransform.localScale.y;
-          playerMultiplier = playerStartingSize * 2;
-          CameraManager.Instance.CameraShake(3, 0.1f);
+            allowedToGrow = true;
+            playerStartingSize = playerTransform.localScale.y;
+            playerMultiplier = playerStartingSize * 2;
+            CameraManager.Instance.CameraShake(3, 0.1f);
+            StartCoroutine(ZoomOut());
         }
 
         if(allowedToGrow)
         {
-
             playerTransform.localScale = new Vector3(playerStartingSize, playerStartingSize, 0);
             playerStartingSize = Mathf.SmoothDamp(playerStartingSize, playerMultiplier, ref velocity, 4.0f);
 
             if (Mathf.Round(playerStartingSize +1f) >= playerMultiplier)
             {
-                CameraManager.Instance.CameraZoom(CameraManager.Instance.cam.orthographicSize * 1.5f);
+                
                 allowedToGrow = false;
             }
         }
+    }
+
+    IEnumerator ZoomOut()
+    {
+        yield return new WaitForSecondsRealtime(3.0f);
+        CameraManager.Instance.CameraZoom(CameraManager.Instance.cam.orthographicSize * 1.5f);
+        StopCoroutine(ZoomOut());
     }
 }
